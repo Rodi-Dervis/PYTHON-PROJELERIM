cf8db52bbf1cb5465a47e8568f3939be4f183073
RODİ DERVİŞ <rodidervis875@gmail.com>
2025-12-24 11:55:05 +0300

    DrivingEligibilityChecker.py
    A simple Python program that checks whether a user is old enough to apply for a driving license.

---
+++ DrivingEligibilityChecker.py
@@ -0,0 +1,8 @@
+ yas=input("lutfen yasinizi giriniz ")
+ sonuc=int(yas)
+
+if sonuc>18:
+     print("ehliyet icin uygun yastasiniz ")
+
+else:
+     print("malesef ehliyeti alacak yasa gelmediniz ")
