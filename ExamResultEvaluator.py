3a5744f6028fdc8ac6f4727e81d05f5d88a5cb19
RODİ DERVİŞ <rodidervis875@gmail.com>
2025-12-24 12:23:18 +0300

    Create ExamResultEvaluator.py
    A simple Python program that calculates the average of midterm and final exam scores to determine whether the student has passed or failed.

---
+++ ExamResultEvaluator.py
@@ -0,0 +1,9 @@
+vize = int(input("lutfen vize notunuzu giriniz  : "))
+final=int(input("lutfen final notunuzu giriniz  : "))
+
+sonuc =(vize +final)/2
+
+if sonuc>=60:
+    print("tebrikler sinavi gectiniz")
+else:
+    print("maalesef sinavda kaldiniz")
