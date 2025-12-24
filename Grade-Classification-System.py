ed0c707a0c69eef4b71dbb54a8545d834d744baf
RODİ DERVİŞ <rodidervis875@gmail.com>
2025-12-24 21:51:34 +0300

    Create Grade-Classification-System.py
    A Python-based automated grading application that evaluates student performance through conditional logic. The system processes numerical input to categorize academic achievements into specific certification tiers based on predefined institutional standards.

---
+++ Grade-Classification-System.py
@@ -0,0 +1,8 @@
+not_degeri=75
+if not_degeri>=85:
+    print("taktir belgesini aldiniz ")
+
+elif not_degeri >=70:
+    print("tesekur belgesini aldiniz")
+else:
+    print("malesef belge alamadiniz")
